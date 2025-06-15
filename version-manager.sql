fba0439828fa31cb346cb734f333780197282d46
zhangpj <874878956@qq.com>
2025-06-15 21:54:51 +0800

    11

--- version-manager.sql
+++ version-manager.sql
@@ -1,3 +1,9 @@
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) DEFAULT NULL,
@@ -14,8 +20,26 @@ CREATE TABLE `files` (
   KEY `version_id` (`version_id`),
   CONSTRAINT `files_ibfk_1` FOREIGN KEY (`uploader_id`) REFERENCES `users` (`id`),
   CONSTRAINT `files_ibfk_2` FOREIGN KEY (`version_id`) REFERENCES `versions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4;
 
+
+CREATE TABLE `operation_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `operation_time` datetime DEFAULT NULL,
+  `operation_type` varchar(50) NOT NULL,
+  `object_type` varchar(50) NOT NULL,
+  `object_name` varchar(100) DEFAULT NULL,
+  `object_id` varchar(50) DEFAULT NULL,
+  `operator_id` int(11) DEFAULT NULL,
+  `details` text,
+  PRIMARY KEY (`id`),
+  KEY `ix_operation_logs_object_id` (`object_id`),
+  KEY `ix_operation_logs_object_type` (`object_type`),
+  KEY `ix_operation_logs_operation_time` (`operation_time`),
+  KEY `ix_operation_logs_operation_type` (`operation_type`),
+  KEY `ix_operation_logs_operator_id` (`operator_id`),
+  CONSTRAINT `operation_logs_ibfk_1` FOREIGN KEY (`operator_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,17 +49,30 @@ CREATE TABLE `products` (
   `status` varchar(20) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `ix_products_code` (`code`)
+  UNIQUE KEY `ix_products_code` (`code`),
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '1',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ix_roles_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 -- version_test.users definition
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) DEFAULT NULL,
-  `name` varchar(32) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
   `email` varchar(120) DEFAULT NULL,
   `password_hash` varchar(128) DEFAULT NULL,
   `is_admin` tinyint(1) DEFAULT NULL,
@@ -49,11 +86,22 @@ CREATE TABLE `users` (
   UNIQUE KEY `ix_users_email` (`email`),
   UNIQUE KEY `ix_users_username` (`username`),
   KEY `fk_users_role_id` (`role_id`),
-  KEY `users_index_0` (`username`,`email`),
   CONSTRAINT `fk_users_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
 
+
+CREATE TABLE `user_product_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `permission_type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `user_product_permissions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_product_permissions_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `versions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -66,31 +114,10 @@ CREATE TABLE `versions` (
   `released_at` datetime DEFAULT NULL,
   `product_id` int(11) NOT NULL,
   `author_id` int(11) NOT NULL,
+  `lock_status` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `author_id` (`author_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `versions_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
   CONSTRAINT `versions_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1',
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `roles_index_0` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE user_product_permissions (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    product_id INT NOT NULL,
-    permission_type VARCHAR(20) NOT NULL, -- 只读: read，编辑: edit，管理: admin
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
-);
